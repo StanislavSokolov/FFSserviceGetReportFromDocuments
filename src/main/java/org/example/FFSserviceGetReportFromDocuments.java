7a686fbcc5f75b05df3f238e8c37ead35a7bdbf0
sokolovstanislav <ignateva-1946@mail.ru>
2025-01-18 14:28:52 +0300

    Calculated the amount of expenses and displayed it on the screen.

--- src/main/java/org/example/FFSserviceGetReportFromDocuments.java
+++ src/main/java/org/example/FFSserviceGetReportFromDocuments.java
@@ -5,15 +5,14 @@ import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileStore;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 public class FFSserviceGetReportFromDocuments {
     public static void main( String[] args ) throws IOException {
         String path = "D:\\Отчеты\\";
         String pathDocuments = path + "Документы\\";
+        double sum = 0;
         if (Files.isDirectory(Paths.get(pathDocuments))) {
             File file = new File(pathDocuments);
             for (File f: file.listFiles()) {
@@ -24,8 +23,23 @@ public class FFSserviceGetReportFromDocuments {
                     text.append(PdfTextExtractor.getTextFromPage(reader, i));
                 }
                 reader.close();
-                System.out.println(text);
+                String st = "";
+                if (text.indexOf("Всего к оплате") != -1) {
+                    // Выделяем строку
+                    st = text.substring(text.indexOf("Всего к оплате"), text.indexOf("Руководитель"));
+                    // Разделяем на слова
+                    String[] rows = st.split(" ");
+                    // Выделяем нужную ячейку
+                    String row = rows[6];
+                    // Меняем знак
+                    row = row.replace(",", ".");
+                    // Суммируем с предыдущим значением
+                    sum = sum + Double.valueOf(row);
+                    System.out.println(f.getName() + " - " + Double.valueOf(row) + " рублей");
+                }
             }
         }
+        System.out.println("");
+        System.out.println("Итого: " + sum + " рублей");
     }
 }
