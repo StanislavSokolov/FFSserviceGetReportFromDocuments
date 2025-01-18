ea972fe54aaead5facd4b092923d38ed20bb758a
sokolovstanislav <ignateva-1946@mail.ru>
2025-01-18 12:19:58 +0300

    Init commit.

---
+++ src/main/java/org/example/FFSserviceGetReportFromDocuments.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class FFSserviceGetReportFromDocuments {
+    public static void main( String[] args )
+    {
+        Update update = new Update();
+        update.start();
+    }
+}
