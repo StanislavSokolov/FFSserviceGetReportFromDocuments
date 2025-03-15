1e6f3efa829a32ca5cd9b4d967b2c963c406a7c5
sokolovstanislav <ignateva-1946@mail.ru>
2025-03-15 14:10:56 +0300

    Added copy files to report. It works good.

--- src/main/java/org/example/FFSserviceGetReportFromDocuments.java
+++ src/main/java/org/example/FFSserviceGetReportFromDocuments.java
@@ -12,34 +12,120 @@ public class FFSserviceGetReportFromDocuments {
     public static void main( String[] args ) throws IOException {
         String path = "D:\\Отчеты\\";
         String pathDocuments = path + "Документы\\";
-        double sum = 0;
+        String pathFiltredDocuments = path + "Отчеты\\";
+
+        String start = "01.01.2025";
+        String stop = "31.03.2025";
+
+        String[] rowsStart = start.split("\\.");
+//        int dayStart = Integer.parseInt(rowsStart[0]);
+        int monthStart = Integer.parseInt(rowsStart[1]);
+        int yearStart = Integer.parseInt(rowsStart[2]);
+
+        String[] rowsStop = stop.split("\\.");
+//        int dayStop = Integer.parseInt(rowsStop[0]);
+        int monthStop = Integer.parseInt(rowsStop[1]);
+        int yearStop = Integer.parseInt(rowsStop[2]);
+
+        String newDir = pathFiltredDocuments + start + "-" + stop;
+        File fileFiltred = new File(newDir);
+
+        if (!fileFiltred.exists()) {
+            boolean isCreated = fileFiltred.mkdirs(); // mkdirs() создает все необходимые промежуточные директории
+            if (isCreated) {
+                System.out.println("Директория для отчета успешно создана: " + newDir);
+            } else {
+                System.out.println("Не удалось создать директорию для отчета: " + newDir);
+            }
+        } else {
+            System.out.println("Директория уже существует: " + newDir);
+            File[] files = fileFiltred.listFiles();
+                if (files != null) {
+                    for (File file: files) {
+                        deleteRecursively(file);
+                    }
+                }
+            System.out.println("Удаляем ее содержимое");
+        }
+
         if (Files.isDirectory(Paths.get(pathDocuments))) {
             File file = new File(pathDocuments);
-            for (File f: file.listFiles()) {
-                PdfReader reader = new PdfReader(pathDocuments + "\\" + f.getName() + "\\" + f.getName() + ".pdf");
-                int pages = reader.getNumberOfPages();
-                StringBuilder text = new StringBuilder();
-                for (int i = 1; i <= pages; i++) {
-                    text.append(PdfTextExtractor.getTextFromPage(reader, i));
-                }
-                reader.close();
-                String st = "";
-                if (text.indexOf("Всего к оплате") != -1) {
-                    // Выделяем строку
-                    st = text.substring(text.indexOf("Всего к оплате"), text.indexOf("Руководитель"));
-                    // Разделяем на слова
+            for (File f : file.listFiles()) {
+                if (f.getName().contains("УПД №")) {
+                    String st = f.getName();
                     String[] rows = st.split(" ");
-                    // Выделяем нужную ячейку
-                    String row = rows[6];
-                    // Меняем знак
-                    row = row.replace(",", ".");
-                    // Суммируем с предыдущим значением
-                    sum = sum + Double.valueOf(row);
-                    System.out.println(f.getName() + " - " + Double.valueOf(row) + " рублей");
+                    if (rows.length == 4) {
+                        String row = rows[rows.length - 1];
+//                        System.out.println(row);
+                        String[] rows1 = row.split("\\.");
+                        int day = Integer.parseInt(rows1[0]);
+                        int month = Integer.parseInt(rows1[1]);
+                        int year = Integer.parseInt(rows1[2]);
+                        if (year >= yearStart & year <= yearStop) {
+                            if (month >= monthStart & month <= monthStop) {
+                                Files.copy(Paths.get(f.getAbsolutePath() + "\\" + f.getName() + ".pdf"), Paths.get(fileFiltred.getAbsolutePath(), f.getName() + ".pdf"));
+                            }
+                        }
+                    }
+
                 }
             }
         }
+
+        int cnt = 0;
+        double sum = 0.0;
+        if (Files.isDirectory(Paths.get(newDir))) {
+            File file = new File(newDir);
+            for (File f: file.listFiles()) {
+                if (f.getName().contains("УПД №")) {
+//                    System.out.println(f.getName());
+                    PdfReader reader = new PdfReader(newDir + "\\" + f.getName());
+                    int pages = reader.getNumberOfPages();
+                    StringBuilder text = new StringBuilder();
+                    for (int i = 1; i <= pages; i++) {
+                        text.append(PdfTextExtractor.getTextFromPage(reader, i));
+                    }
+                    reader.close();
+                    String st = "";
+                    if (text.indexOf("Всего к оплате") != -1) {
+                        // Выделяем строку
+                        st = text.substring(text.indexOf("Всего к оплате"), text.indexOf("Руководитель"));
+//                        System.out.println(st);
+                        // Разделяем на слова
+                        String[] rows = st.split(" ");
+                        // Выделяем нужную ячейку
+                        String row = rows[rows.length - 1];
+//                        System.out.println(row);
+                        String[] rows1 = row.split("\\n");
+//                        System.out.println("dfv" + rows1[0]);
+                        // Меняем знак
+                        rows1[0] = rows1[0].replace(",", ".");
+                        // Суммируем с предыдущим значением
+//                        System.out.println(rows1[0]);
+//                        System.out.println(Double.valueOf(rows1[0]));
+                        sum = sum + Double.valueOf(rows1[0]);
+                        System.out.println(f.getName() + " - " + Double.valueOf(rows1[0]) + " рублей");
+                    }
+                }
+            }
+            cnt = file.listFiles().length;
+        }
         System.out.println("");
         System.out.println("Итого: " + sum + " рублей");
+        System.out.println("Просмотрено " + cnt + " документов");
+    }
+
+    private static void deleteRecursively(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File subFile : files) {
+                    deleteRecursively(subFile);
+                }
+            }
+        }
+        if (!file.delete()) {
+            System.err.println("Не удалось удалить файл или директорию: " + file.getAbsolutePath());
+        }
     }
 }
